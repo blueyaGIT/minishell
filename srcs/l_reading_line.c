3da5d798d05fd4578863b994bc53ef5187cd1294
Laura Nierobisch <lnierobi@2-F-4.42heilbronn.de>
2025-02-08 13:45:29 +0100

    fixed some norm

--- srcs/l_reading_line.c
+++ srcs/l_reading_line.c
@@ -1,52 +1,64 @@
 #include "../includes/minishell.h"
-void print_tokens(char **token)
+
+void	print_tokens(char **token)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (token[i])
 	{
 		printf("Token[%d]: %s\n", i, token[i]);
 		i++;
 	}
 }
-char *create_quote_token(char **start)
+char	*create_quote_token(char **start)
 {
-	char quote = **start;
-	char *end;
+	char	quote;
+	char	*end;
+	char	*token;
+
+	quote = **start;
 	(*start)++;
 	end = *start;
 	while (*end && *end != quote)
 		end++;
-	char *token = ft_strndup(*start, end - *start);
+	token = ft_strndup(*start, end - *start);
 	if (*end == quote)
 		*start = end + 1;
 	else
 		*start = end;
-	return token;
+	return (token);
 }
-char *create_word_token(char **start)
+char	*create_word_token(char **start)
 {
-			char *end = *start;
-			while (*end && *end != '<' && *end != '>' && *end != '|' && *end != ' ')
-				end++;
-			char *token = ft_strndup(*start, end - *start);
-			*start = end;
-	return token;
+	char	*end;
+	char	*token;
+
+	end = *start;
+	while (*end && *end != '<' && *end != '>' && *end != '|' && *end != ' ')
+		end++;
+	token = ft_strndup(*start, end - *start);
+	*start = end;
+	return (token);
 }
-char **create_token(t_mini mini)
+char	**create_token(t_mini mini)
 {
-	int num = 0;
-	char *start = mini.input;
-	// char *end;
-	mini.tokens = malloc(sizeof(char *) * 100);
+	int		num;
+	char	*start;
+
+	num = 0;
+	start = mini.input;
+	mini.tokens = malloc(sizeof(char *) * ft_strlen(start));
 	if (!mini.tokens)
-		return NULL;
+		return (NULL);
 	while (*start)
 	{
 		while (*start == ' ')
 			start++;
 		if (*start == '"' || *start == '\'')
 			mini.tokens[num++] = create_quote_token(&start);
-		else if ((*start == '<' && start[1] == '<') || (*start == '>' && start[1] == '>'))
+		else if ((*start == '<' && start[1] == '<') || (*start == '>'
+				&& start[1] == '>'))
 		{
 			mini.tokens[num++] = ft_strndup(start, 2);
 			start += 2;
@@ -57,8 +69,8 @@ char **create_token(t_mini mini)
 			start++;
 		}
 		else
-			mini.tokens[num++] =create_word_token(&start);
+			mini.tokens[num++] = create_word_token(&start);
 	}
-	mini.tokens[num] = NULL;
-	return mini.tokens;
+	return (mini.tokens[num] = NULL, mini.tokens);
 }
+
