5695a4a7787f05e0072bac6bb8d81d83bb7fbfab
Laura Nierobisch <lnierobi@2-F-4.42heilbronn.de>
2025-02-08 11:42:17 +0100

    added quotes in tokenizer

--- srcs/l_reading_line.c
+++ srcs/l_reading_line.c
@@ -8,12 +8,35 @@ void print_tokens(char **token)
 		i++;
 	}
 }
-
+char *create_quote_token(char **start)
+{
+	char quote = **start;
+	char *end;
+	(*start)++;
+	end = *start;
+	while (*end && *end != quote)
+		end++;
+	char *token = ft_strndup(*start, end - *start);
+	if (*end == quote)
+		*start = end + 1;
+	else
+		*start = end;
+	return token;
+}
+char *create_word_token(char **start)
+{
+			char *end = *start;
+			while (*end && *end != '<' && *end != '>' && *end != '|' && *end != ' ')
+				end++;
+			char *token = ft_strndup(*start, end - start);
+			*start = end;
+	return token;
+}
 char **create_token(t_mini mini)
 {
-	int i = 0;
+	int num = 0;
 	char *start = mini.input;
-	char *end;
+	// char *end;
 	mini.tokens = malloc(sizeof(char *) * 100);
 	if (!mini.tokens)
 		return NULL;
@@ -21,25 +44,21 @@ char **create_token(t_mini mini)
 	{
 		while (*start == ' ')
 			start++;
-		if ((*start == '<' && start[1] == '<') || (*start == '>' && start[1] == '>'))
+		if (*start == '"' || *start == '\'')
+			mini.tokens[num++] = create_quote_token(&start);
+		else if ((*start == '<' && start[1] == '<') || (*start == '>' && start[1] == '>'))
 		{
-			mini.tokens[i++] = strndup(start, 2);
+			mini.tokens[num++] = ft_strndup(start, 2);
 			start += 2;
 		}
 		else if (*start == '|' || *start == '<' || *start == '>')
 		{
-			mini.tokens[i++] = strndup(start, 1);
+			mini.tokens[num++] = ft_strndup(start, 1);
 			start++;
 		}
 		else
-		{
-			end = start;
-			while (*end && *end != '<' && *end != '>' && *end != '|' && *end != ' ')
-				end++;
-			mini.tokens[i++] = strndup(start, end - start);
-			start = end;
-		}
+			mini.tokens[num++] =create_word_token(&start);
 	}
-	mini.tokens[i] = NULL;
+	mini.tokens[num] = NULL;
 	return mini.tokens;
 }
