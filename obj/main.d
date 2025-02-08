c6a9c970602771b0708180e490cc404c71cad889
blueyaGIT <albanodavide50@gmail.com>
2025-02-08 13:11:19 +0100

    updated asciiart

---
+++ obj/main.d
@@ -0,0 +1,33 @@
+obj/main.o: srcs/main.c srcs/../includes/minishell.h \
+  srcs/../includes/libft/libft.h \
+  srcs/../includes/libft/include/get_next_line/get_next_line.h \
+  srcs/../includes/libft/include/printf/ft_printf.h \
+  /usr/local/include/readline/readline.h \
+  /usr/local/include/readline/rlstdc.h \
+  /usr/local/include/readline/rltypedefs.h \
+  /usr/local/include/readline/keymaps.h \
+  /usr/local/include/readline/chardefs.h \
+  /usr/local/include/readline/tilde.h \
+  /usr/local/include/readline/history.h
+
+srcs/../includes/minishell.h:
+
+srcs/../includes/libft/libft.h:
+
+srcs/../includes/libft/include/get_next_line/get_next_line.h:
+
+srcs/../includes/libft/include/printf/ft_printf.h:
+
+/usr/local/include/readline/readline.h:
+
+/usr/local/include/readline/rlstdc.h:
+
+/usr/local/include/readline/rltypedefs.h:
+
+/usr/local/include/readline/keymaps.h:
+
+/usr/local/include/readline/chardefs.h:
+
+/usr/local/include/readline/tilde.h:
+
+/usr/local/include/readline/history.h:
