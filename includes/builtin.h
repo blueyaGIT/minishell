00486652e3bf55be76a8e8b9ec071ef6b8009cb2
blueyaGIT <albanodavide50@gmail.com>
2025-03-26 15:00:45 +0100

    added exec_exit to bultin.h & //ft_exec

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 02:11:24 by dalbano           #+#    #+#             */
-/*   Updated: 2025/03/25 18:14:09 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/03/26 14:59:03 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "minishell.h"
 
 int	exec_env(t_shell *shell, char **args);
+int	exec_exit(t_shell *shell, char **args);
 
 #endif /* BUILTIN_H */
