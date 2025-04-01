259bdef9a7181ed469cfbb144a45ddddf2d9aa89
blueyaGIT <albanodavide50@gmail.com>
2025-04-01 15:45:34 +0200

    wrong exit in children.c -> check_children

--- srcs/main.c
+++ srcs/main.c
@@ -1,37 +1,37 @@
 #include "minishell.h"
 
-volatile sig_atomic_t g_ecode = 0;
+volatile sig_atomic_t	g_ecode = 0;
 
-static void initialize_shell(t_shell *shell, char **argv, char **envp)
+static void	initialize_shell(t_shell *shell, char **argv, char **envp)
 {
-	(void) argv;
-    ft_memset(shell, 0, sizeof(t_shell));
-    if (shell_init(shell, envp) != success)
-        exit(EXIT_FAILURE);
-    ft_print_logo(envp);
-    print_shell(shell);
+	(void)argv;
+	ft_memset(shell, 0, sizeof(t_shell));
+	if (shell_init(shell, envp) != success)
+		exit(EXIT_FAILURE);
+	ft_print_logo(envp);
+	print_shell(shell);
 }
 
-int main(int argc, char *argv[], char **envp)
+int	main(int argc, char *argv[], char **envp)
 {
 	t_shell	shell;
 
 	if (argc != 1)
-		return (printf(RED"ERROR Input format: ./minishell\n"RESET));
+		return (printf(RED "ERROR Input format: ./minishell\n" RESET));
 	initialize_shell(&shell, argv, envp);
 	while (1)
 	{
 		init_signals();
 		shell.input = readline(PROMPT);
 		if (!shell.input)
-			break;
+			break ;
 		process_input(&shell);
 		handle_syntax_and_exit(&shell);
 		execute_commands(&shell);
 		refresh_signals();
 		if (ft_strcmp(shell.input, "./minishell") == 0)
 			check_shlvl(&shell);
-		// g_ecode = ft_exec(&shell);
+		g_ecode = ft_exec(&shell);
 		refresh_shell(&shell);
 	}
 	kill_shell(&shell, 1);
