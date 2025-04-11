383a31c1003b96a563eaa32cfddeecd55e6eb163
blueyaGIT <albanodavide50@gmail.com>
2025-04-11 15:48:01 +0200

    added pwd builtin command

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 02:11:24 by dalbano           #+#    #+#             */
-/*   Updated: 2025/03/26 14:59:03 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/11 15:45:32 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 
 int	exec_env(t_shell *shell, char **args);
 int	exec_exit(t_shell *shell, char **args);
+int	exec_pwd(t_shell *shell);
 
 #endif /* BUILTIN_H */
--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,8 +6,28 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 15:09:03 by dalbano           #+#    #+#             */
-/*   Updated: 2025/04/11 15:09:03 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/11 15:45:20 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	exec_pwd(t_shell *shell)
+{
+	char	buf[PATH_MAX];
+	char	*cwd;
+
+	if (shell->cur_dir)
+	{
+		ft_fprintf(STDOUT_FILENO, "%s\n", shell->cur_dir);
+		return (EXIT_SUCCESS);
+	}
+	cwd = getcwd(buf, PATH_MAX);
+	if (cwd)
+	{
+		ft_fprintf(STDOUT_FILENO, "%s\n", cwd);
+		return (EXIT_SUCCESS);
+	}
+	print_error("PWD", shell);
+	return (EXIT_FAILURE);
+}
