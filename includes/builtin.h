f677ebcf26cd9417f30b564b81f282152af883e9
blueyaGIT <albanomarzia50@gmail.com>
2025-04-12 15:25:11 +0200

    added builtin-echo

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 02:11:24 by dalbano           #+#    #+#             */
-/*   Updated: 2025/04/12 14:38:05 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/04/12 15:23:55 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	exec_env(t_shell *shell, char **args);
 int	exec_exit(t_shell *shell, char **args);
 int	exec_pwd(t_shell *shell);
 int	exec_cd(t_shell *shell, char **args);
+int	exec_echo(char **args);
 
 #endif /* BUILTIN_H */
