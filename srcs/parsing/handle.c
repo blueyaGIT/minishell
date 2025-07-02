af50466584a452bd0e9c692486bfcd8ba16204f8
lilikts <lilikts@icloud.com>
2025-07-02 14:59:57 +0200

    parsing-lexer: empty quote check + handling

--- srcs/parsing/handle.c
+++ srcs/parsing/handle.c
@@ -6,7 +6,7 @@
 /*   By: lkloters <lkloters@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 15:45:13 by lkloters          #+#    #+#             */
-/*   Updated: 2025/07/02 11:04:05 by lkloters         ###   ########.fr       */
+/*   Updated: 2025/07/02 14:59:03 by lkloters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,12 @@ void handle_pipe(t_token **token, int *i)
 {
 	token_add_back(token, new_token(T_PIPE, ft_strdup("|")));
 	(*i)++;
+}
+
+void handle_empty_quote(t_token **token, int *i)
+{
+	token_add_back(token, new_token(T_WORD, ft_strdup("")));
+	(*i) += 2;
 }
 
 void handle_word(t_token **token, char *input, int *i)
--- srcs/parsing/lexer.c
+++ srcs/parsing/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: lkloters <lkloters@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 13:43:01 by lkloters          #+#    #+#             */
-/*   Updated: 2025/07/02 14:16:43 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/07/02 14:59:11 by lkloters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,42 @@
 
 bool is_redirectory(char c)
 {
-	if (c == '<' || c == '>' || c == '<<' || c == '>>')
+	if (c == '<' || c == '>')
 		return (true);
 	return (false);
 }
 
-bool is_empty_quote(char *input, int *i)
+bool before_is_word(char *input, int i)
+{
+	unsigned char c;
+	if (i <= 0)
+		return (false);
+		c = (unsigned char)input[i - 1];
+	if (ft_isalnum(c) || c == '_')
+		return (true);
+	return (false);
+}
+bool after_is_word(char *input, int i)
+{
+	unsigned char c;
+	if (input[i + 1] == '\0')
+		return (false);
+	c = (unsigned char)input[i + 1];
+	if (ft_isalnum(c) || c == '_')
+		return (true);
+	return (false);
+}
+
+bool is_empty_quote(char *input, int i)
 {
 	char c;
-	c = input[*i];
-	if ((c == '\'' || c == '\"') && input[*i + 1] == c)
+	c = input[i];
+	if ((c == '\'' || c == '\"') && input[i + 1] == c)
 	{
-		if (before_is_word(input, *i) && after_is_word(input, *i))
+		if (before_is_word(input, i) || after_is_word(input, i))
 			return (false);
-		return (true);
+		else
+			return (true);
 	}
 	return (false);
 }
@@ -45,8 +67,8 @@ t_token *lexer(char *input)
 			handle_pipe(&token, &i);
 		else if (is_redirectory(input[i]))
 			handle_redirectory(&token, input, &i);
-		else if (is_empty_quote)
-			handle_empty_quote();
+		else if (is_empty_quote(input, i))
+			handle_empty_quote(&token, &i);
 		else
 			handle_word()
 	}
