185127ba4a1c1455918f2f58ba42ef8e47002d2f
lilikts <lilikts@icloud.com>
2025-07-16 10:42:17 +0200

    wip

---
+++ obj/handle_expansion.d
@@ -0,0 +1,63 @@
+obj/handle_expansion.o: srcs/lexer/handle_expansion.c \
+  includes/minishell.h includes/libft/libft.h \
+  includes/libft/include/fprintf/ft_fprintf.h \
+  includes/libft/include/get_next_line/get_next_line.h \
+  includes/libft/include/printf/ft_printf.h includes/shell.h \
+  includes/builtin.h includes/debug.h includes/env.h includes/exec.h \
+  includes/error.h includes/format.h includes/io.h includes/parsing.h \
+  includes/pipes.h includes/signals.h includes/utils.h \
+  /usr/local/include/readline/history.h \
+  /usr/local/include/readline/rlstdc.h \
+  /usr/local/include/readline/rltypedefs.h \
+  /usr/local/include/readline/readline.h \
+  /usr/local/include/readline/keymaps.h \
+  /usr/local/include/readline/chardefs.h \
+  /usr/local/include/readline/tilde.h
+
+includes/minishell.h:
+
+includes/libft/libft.h:
+
+includes/libft/include/fprintf/ft_fprintf.h:
+
+includes/libft/include/get_next_line/get_next_line.h:
+
+includes/libft/include/printf/ft_printf.h:
+
+includes/shell.h:
+
+includes/builtin.h:
+
+includes/debug.h:
+
+includes/env.h:
+
+includes/exec.h:
+
+includes/error.h:
+
+includes/format.h:
+
+includes/io.h:
+
+includes/parsing.h:
+
+includes/pipes.h:
+
+includes/signals.h:
+
+includes/utils.h:
+
+/usr/local/include/readline/history.h:
+
+/usr/local/include/readline/rlstdc.h:
+
+/usr/local/include/readline/rltypedefs.h:
+
+/usr/local/include/readline/readline.h:
+
+/usr/local/include/readline/keymaps.h:
+
+/usr/local/include/readline/chardefs.h:
+
+/usr/local/include/readline/tilde.h:
