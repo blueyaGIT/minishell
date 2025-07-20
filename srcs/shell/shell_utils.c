5160711657878b6d0a4323ca84cf14a04317f171
blueyaGIT <albanomarzia50@gmail.com>
2025-07-20 15:45:27 +0200

    added t_token free to reload_shell

--- srcs/shell/shell_utils.c
+++ srcs/shell/shell_utils.c
@@ -7,6 +7,8 @@ void	reload_shell(t_shell *shell)
 		free(shell->input);
 		shell->input = NULL;
 	}
+	if (shell && shell->token)
+		free_token(shell->token);
 	if (shell && shell->tokens)
 		ft_free_arr(shell->tokens);
 	if (shell && shell->cmd_list)
