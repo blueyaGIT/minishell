54745ca4c50dbd032ecfccf6f7625e7e59af8de7
blueyaGIT <albanomarzia50@gmail.com>
2025-07-20 15:27:13 +0200

    added pipe-free into reload_shell

--- srcs/shell/shell_utils.c
+++ srcs/shell/shell_utils.c
@@ -10,7 +10,10 @@ void	reload_shell(t_shell *shell)
 	if (shell && shell->tokens)
 		ft_free_arr(shell->tokens);
 	if (shell && shell->cmd_list)
+	{
+		kill_pipes(shell->cmd_list, NULL);
 		ft_free_command(shell->cmd_list);
+	}
 }
 
 void	ft_free_shell(t_shell *shell)
@@ -20,6 +23,8 @@ void	ft_free_shell(t_shell *shell)
 		free(shell->input);
 		shell->input = NULL;
 	}
+	if (shell && shell->token)
+		free_token(shell->token);
 	if (shell && shell->tokens)
 		ft_free_arr(shell->tokens);
 	if (shell && shell->cmd_list)
