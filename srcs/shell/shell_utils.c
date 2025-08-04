268e060914c41a1b08e0a29edb6d12d83fe2edd0
blueyaGIT <albanomarzia50@gmail.com>
2025-08-04 12:53:29 +0200

    --wip-- [skip ci]

--- srcs/shell/shell_utils.c
+++ srcs/shell/shell_utils.c
@@ -2,41 +2,63 @@
 
 void	reload_shell(t_shell *shell)
 {
-	if (shell && shell->input)
-	{
-		free(shell->input);
-		shell->input = NULL;
-	}
-	if (shell && shell->token)
-		free_token(shell->token);
-	if (shell && shell->tokens)
-		ft_free_arr(shell->tokens);
-	if (shell && shell->cmd_list)
-	{
-		kill_pipes(shell->cmd_list, NULL);
-		ft_free_command(shell->cmd_list);
-	}
+    t_command	*current;
+    t_command	*temp;
+
+    if (shell && shell->input)
+    {
+        free(shell->input);
+        shell->input = NULL;
+    }
+    if (shell && shell->token)
+        free_token(shell->token);
+    if (shell && shell->tokens)
+        ft_free_arr(shell->tokens);
+    if (shell && shell->cmd_list)
+    {
+        kill_pipes(shell->cmd_list, NULL);
+        current = shell->cmd_list;
+        while (current->prev)
+            current = current->prev;
+        while (current)
+        {
+            temp = current->next;
+            ft_free_command(current);
+            current = temp;
+        }
+    }
 }
 
 void	ft_free_shell(t_shell *shell)
 {
-	if (shell && shell->input)
-	{
-		free(shell->input);
-		shell->input = NULL;
-	}
-	if (shell && shell->token)
-		free_token(shell->token);
-	if (shell && shell->tokens)
-		ft_free_arr(shell->tokens);
-	if (shell && shell->cmd_list)
-	{
-		kill_pipes(shell->cmd_list, NULL);
-		ft_free_command(shell->cmd_list);
-	}
-	if (shell && shell->env)
-		ft_free_env(shell->env);
-	rl_clear_history();
+    t_command	*current;
+    t_command	*temp;
+
+    if (shell && shell->input)
+    {
+        free(shell->input);
+        shell->input = NULL;
+    }
+    if (shell && shell->token)
+        free_token(shell->token);
+    if (shell && shell->tokens)
+        ft_free_arr(shell->tokens);
+    if (shell && shell->cmd_list)
+    {
+        kill_pipes(shell->cmd_list, NULL);
+        current = shell->cmd_list;
+        while (current->prev)
+            current = current->prev;
+        while (current)
+        {
+            temp = current->next;
+            ft_free_command(current);
+            current = temp;
+        }
+    }
+    if (shell && shell->env)
+        ft_free_env(shell->env);
+    rl_clear_history();
 }
 
 t_shell	*get_shell(void)
