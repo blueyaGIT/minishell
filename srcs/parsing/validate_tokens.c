3e4bf2b9f4105a8994de279b3f6f9a02f0ec668e
blueyaGIT <albanomarzia50@gmail.com>
2025-09-04 15:21:17 +0200

    added comment

--- srcs/parsing/validate_tokens.c
+++ srcs/parsing/validate_tokens.c
@@ -24,7 +24,7 @@ static bool	valid_redirection_syntax(t_token *token)
 {
 	if (!token)
 		return (false);
-	if (is_redirection_token(token))
+	if (is_redirection_token(token)) //WTFFFF
 		return (false);
 	while (token)
 	{
