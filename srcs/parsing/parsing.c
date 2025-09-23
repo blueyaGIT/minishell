97dc7f41309a2a3bd1c0226620ca4fa45d4e3706
Lilli Kloters <lkloters@3-F-12.42heilbronn.de>
2025-09-23 17:06:43 +0200

    heredocs sruct almost done

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,35 +6,35 @@
 /*   By: lkloters <lkloters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 14:47:02 by dalbano           #+#    #+#             */
-/*   Updated: 2025/09/23 15:12:17 by lkloters         ###   ########.fr       */
+/*   Updated: 2025/09/23 16:32:01 by lkloters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void print_tokens(t_token *head)
-{
-    t_token *current = head;
-    int index = 0;
+// static void print_tokens(t_token *head)
+// {
+//     t_token *current = head;
+//     int index = 0;
 
-    printf("------ TOKEN LIST ------\n");
-    while (current)
-    {
-        printf("Token[%d]:\n", index);
-        printf("  value      : %s\n", current->value ? current->value : "NULL");
-        printf("  type       : %d\n", current->type);
-        printf("  is_echo    : %s\n", current->is_echo ? "true" : "false");
-        printf("  is_echo_n  : %s\n", current->is_echo_n ? "true" : "false");
-        printf("  is_removed : %s\n", current->is_removed ? "true" : "false");
-        printf("  add_space  : %s\n", current->add_space ? "true" : "false");
-        printf("  next       : %p\n", (void *)current->next);
-        printf("  prev       : %p\n", (void *)current->prev);
-        printf("------------------------\n");
+//     printf("------ TOKEN LIST ------\n");
+//     while (current)
+//     {
+//         printf("Token[%d]:\n", index);
+//         printf("  value      : %s\n", current->value ? current->value : "NULL");
+//         printf("  type       : %d\n", current->type);
+//         printf("  is_echo    : %s\n", current->is_echo ? "true" : "false");
+//         printf("  is_echo_n  : %s\n", current->is_echo_n ? "true" : "false");
+//         printf("  is_removed : %s\n", current->is_removed ? "true" : "false");
+//         printf("  add_space  : %s\n", current->add_space ? "true" : "false");
+//         printf("  next       : %p\n", (void *)current->next);
+//         printf("  prev       : %p\n", (void *)current->prev);
+//         printf("------------------------\n");
 
-        current = current->next;
-        index++;
-    }
-}
+//         current = current->next;
+//         index++;
+//     }
+// }
 
 static t_token *create_pipe_token(void)
 {
@@ -57,6 +57,26 @@ static t_token *create_pipe_token(void)
     return pipe;
 }
 
+static t_token *create_cat_token(void)
+{
+    t_token *cat = malloc(sizeof(t_token));
+    if (!cat)
+        return NULL;
+    cat->value = ft_strdup("cat");
+    if (!cat->value)
+    {
+        free(cat);
+        return NULL;
+    }
+    cat->type = 7;
+    cat->is_echo = 0;
+    cat->is_echo_n = 0;
+    cat->is_removed = 0;
+    cat->add_space = 0;
+    cat->next = NULL;
+    cat->prev = NULL;
+    return cat;
+}
 
 static int insert_pipe(t_token *token)
 {
@@ -68,13 +88,27 @@ static int insert_pipe(t_token *token)
             current->next->type == HEREDOC_DELIM && current->next->next->type != T_PIPE)
         {
             t_token *pipe = create_pipe_token();
-            if (!pipe)
+			t_token *cat = create_cat_token();
+            if (!pipe || !cat)
+            {
+                if (pipe) free(pipe);
+                if (cat) free(cat);
                 return (0);
+            }
+             t_token *next_token = current->next->next;
+            
+            // Insert pipe after current->next (delimiter)
             pipe->prev = current->next;
-            pipe->next = current->next->next;
-            current->next->next->prev = pipe;
+            pipe->next = cat;
             current->next->next = pipe;
-            current = pipe->next;
+            
+            // Insert cat after pipe
+            cat->prev = pipe;
+            cat->next = next_token;
+            next_token->prev = cat;
+            
+            // Move current past the inserted tokens
+            current = next_token;
         }
         else
             current = current->next;
@@ -96,7 +130,8 @@ int	process_and_execute_input(t_shell *shell)
 		return (0);
 	}
 	insert_pipe(token);
-	print_tokens(token);
+	
+	// print_tokens(token);
 	shell->token = token;
 	command = parse_code(shell->token);
 	if (!command && shell->token)
@@ -127,3 +162,4 @@ t_command	*parse_code(t_token *token)
 	}
 	return (command);
 }
+
